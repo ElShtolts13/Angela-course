210d01a7af1493685fe4c2fbb8074fa31d9e943e
Nadezhda Kapatsina <nadezhda.kapatsina@gmail.com>
2023-11-04 20:24:46 +0300

    Lesson 8. Egg Timer Complete

--- Lesson 8. Egg Timer/Egg Timer/Egg Timer/ViewController.swift
+++ Lesson 8. Egg Timer/Egg Timer/Egg Timer/ViewController.swift
@@ -6,23 +6,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
-
+    @IBOutlet weak var tittleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var timer = Timer()
+    
+    var totalTime = 0
+    var secondsPass = 0
+    
+    var player: AVAudioPlayer!
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-       
-        let harndess = sender.currentTitle!
-        var secondsRemaining = eggTimes[harndess]!
         
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-            if secondsRemaining > 0 {
-                print ("\(secondsRemaining) seconds")
-                secondsRemaining -= 1
+        timer.invalidate()
+        
+        let harndess = sender.currentTitle!
+        totalTime = eggTimes[harndess]!
+        progressBar.progress = 0.0
+        secondsPass = 0
+        tittleLabel.text = harndess
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsPass <= self.totalTime {
+                
+                self.progressBar.progress = Float(self.secondsPass)/Float(self.totalTime)
+                
+                self.secondsPass += 1
             } else {
                 Timer.invalidate()
+                self.tittleLabel.text = "Done!"
+                self.playSound()
             }
         }
-        }
     }
+    
+    func playSound(){
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+}
+
