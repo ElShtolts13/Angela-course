f7eb56ff32b0d0da7ed3d9effdbc629dacc59758
Nadezhda Kapatsina <nadezhda.kapatsina@gmail.com>
2023-11-04 13:33:43 +0300

    Lesson 8. Fix problem with button title

--- Lesson 8. Egg Timer/Egg Timer/Egg Timer/ViewController.swift
+++ Lesson 8. Egg Timer/Egg Timer/Egg Timer/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        print(sender.currentTitle)
     }
+    
 
-
+    
+    
 }
 
