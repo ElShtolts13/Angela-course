bb6c8efff7463d95943d090ca6f0adfe126943ca
Nadezhda Kapatsina <nadezhda.kapatsina@gmail.com>
2023-11-04 16:41:29 +0300

    Lesson 8. Add countdown timer

--- Lesson 8. Egg Timer/Egg Timer/Egg Timer/ViewController.swift
+++ Lesson 8. Egg Timer/Egg Timer/Egg Timer/ViewController.swift
@@ -8,13 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle)
+       
+        let harndess = sender.currentTitle!
+        var secondsRemaining = eggTimes[harndess]!
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if secondsRemaining > 0 {
+                print ("\(secondsRemaining) seconds")
+                secondsRemaining -= 1
+            } else {
+                Timer.invalidate()
+            }
+        }
+        }
     }
-    
-
-    
-    
-}
-
